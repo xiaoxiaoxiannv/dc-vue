0f18b282fb73d9095ba788a7428af223fc9bc10e
Luna <1017929760@qq.com>
2020-07-09 14:24:15 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div>当且页面不存在,请检查网址是否正确</div>
+        <div>
+            <router-link to="/">返回首页</router-link>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NotFound extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

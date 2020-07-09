3f21f846f27c1fe15d8f515b162ecc4a74ba9d8b
Luna <1017929760@qq.com>
2020-07-09 13:57:15 +0800

    创建三个页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>L</div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Labels extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>S</div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Statistics extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

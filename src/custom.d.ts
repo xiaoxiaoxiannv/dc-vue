68cfcf95614ad3f66af551f6853f8c87d7938341
Luna <1017929760@qq.com>
2020-07-15 21:27:37 +0800

    将所有数据操作封装在store里

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -17,13 +17,3 @@ type TagListModel = {
   remove: (id: string) => boolean;
   save: () => void;
 }
-
-interface Window {
-  tagList: Tag[];
-  findTag: (id: string) => Tag | undefined;
-  createTag: (name: string) => void;
-  removeTag: (id: string) => boolean;
-  updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
-  recordList: RecordItem[];
-  createRecord: (record: RecordItem) => void;
-}

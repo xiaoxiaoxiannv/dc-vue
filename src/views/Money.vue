a55da5c23d637da41aa07e0079281cf783167b59
Luna <1017929760@qq.com>
2020-07-17 16:44:03 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,13 +18,12 @@
   import NumberPad from '@/components/Money/NumberPad.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import Types from '@/components/Money/Types.vue';
   import {Component} from 'vue-property-decorator';
   import recordTypeList from '@/constants/recordTypeList';
   import Tabs from '@/components/Money/Tabs.vue';
 
   @Component({
-    components: {Tabs, Types, Tags, FormItem, NumberPad}
+    components: {Tabs, Tags, FormItem, NumberPad}
   })
   export default class Money extends Vue {
     get recordList() {

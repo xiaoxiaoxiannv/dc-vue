ac0490663198dbc90ef64941850a9a835efa32ac
Luna <1017929760@qq.com>
2020-07-17 13:22:14 +0800

    修复刷新变成404的bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
 import createId from '@/lib/createId';
+import router from '@/router';
 
 Vue.use(Vuex);
 
@@ -17,9 +18,6 @@ const store = new Vuex.Store({
     currentTag: undefined
   } as RootState,
   mutations: {
-    setCurrentTag(state,id: string){
-      state.currentTag = state.tagList.filter(t =>t.id === id)[0];
-    },
     fetchRecord(state) {
       state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
     },
@@ -37,6 +35,9 @@ const store = new Vuex.Store({
     fetchTag(state) {
       state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
     },
+    setCurrentTag(state, id: string) {
+      state.currentTag = state.tagList.filter(t => t.id === id)[0];
+    },
     createTag(state, name: string) {
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
@@ -46,6 +47,36 @@ const store = new Vuex.Store({
       state.tagList.push({id, name: name});
       store.commit('saveTag');
       window.alert('标签创建成功');
+    },
+    updateTag(state, object: { id: string; name: string }) {
+      const {id, name} = object;
+      const idList = state.tagList.map(item => item.id);
+      if (idList.indexOf(id) >= 0) {
+        const names = state.tagList.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+          window.alert('标签名不能重复');
+        } else {
+          const tag = state.tagList.filter(item => item.id === id)[0];
+          tag.name = name;
+          store.commit('saveTag');
+        }
+      }
+    },
+    removeTag(state, id: string) {
+      let index = -1;
+      for (let i = 0; i < state.tagList.length; i++) {
+        if (state.tagList[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      if (index >= 0) {
+        state.tagList.splice(index, 1);
+        store.commit('saveTag');
+        router.back();
+      } else {
+        window.alert('删除失败');
+      }
     },
     saveTag(state) {
       window.localStorage.setItem('tagList',
